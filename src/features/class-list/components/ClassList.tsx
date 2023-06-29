d0bbda6278f32d030095f00265149fb3f4943790
Abbondanzo <peter@abbondanzo.com>
2023-06-29 02:52:48 -0400

    Tooltip for no classes

--- src/features/class-list/components/ClassList.tsx
+++ src/features/class-list/components/ClassList.tsx
@@ -4,12 +4,18 @@ import { ClassListItem } from "./ClassListItem";
 import { useAppSelector } from "../../store/hooks/useStore";
 import { selectFilteredClasses } from "../selectors/selectFilteredClasses";
 import { Card } from "./atoms/Card";
+import { selectHasFilters } from "../../filters/selectors/selectHasFilters";
 
 const Wrapper = styled.div`
   & > *:not(:last-child) {
     display: block;
     margin-bottom: 8px;
   }
+`;
+
+const TipText = styled.i`
+  margin-top: 4px;
+  font-size: 12px;
 `;
 
 interface Props {
@@ -20,11 +26,18 @@ export const ClassList = ({ classes }: Props) => {
   const filteredClasses = useAppSelector((state) =>
     selectFilteredClasses(state, classes)
   );
+  const hasFilters = useAppSelector(selectHasFilters);
 
   if (filteredClasses.length === 0) {
     return (
       <Card>
         <p>No classes!</p>
+        {hasFilters && (
+          <TipText>
+            Tip: Try resetting your filters or selecting more options in the
+            sidebar.
+          </TipText>
+        )}
       </Card>
     );
   }
---
+++ src/features/filters/selectors/selectHasFilters.ts
@@ -0,0 +1,8 @@
+import { RootState } from "./../../store/constants/store";
+
+export const selectHasFilters = (state: RootState) => {
+  return (
+    state.filters.selectedDisciplines.length > 0 ||
+    state.filters.selectedInstructors.length > 0
+  );
+};
