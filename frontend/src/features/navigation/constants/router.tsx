b2d2f49fbe4b826920bd7779398b7294ccb514d7
Abbondanzo <peter@abbondanzo.com>
2023-07-10 00:05:20 -0400

    Use coming soon card

--- frontend/src/features/navigation/constants/router.tsx
+++ frontend/src/features/navigation/constants/router.tsx
@@ -4,9 +4,30 @@ import {
   Navigate,
   Route,
 } from "react-router-dom";
+import styled from "styled-components";
 import { ClassListRoot } from "../../class-list/components/ClassListRoot";
-import { SignInRoot } from "../../session/components/SignInRoot";
+import { NavbarProvider } from "../components/NavbarProvider";
 import { Paths } from "./paths";
+import { Card } from "../../theme/components/Card";
+
+const ComingSoonWrapper = styled.div`
+  max-width: 960px;
+  padding: 1em;
+  margin: 0 auto;
+`;
+
+const ComingSoon = () => {
+  return (
+    <NavbarProvider>
+      <ComingSoonWrapper>
+        <Card>
+          <h1>Coming Soon</h1>
+          <p>We're still working on this feature. Please bear with us!</p>
+        </Card>
+      </ComingSoonWrapper>
+    </NavbarProvider>
+  );
+};
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,7 +37,8 @@ export const router = createBrowserRouter(
         element={<Navigate to={Paths.CLASS_LIST} replace={true} />}
       />
       <Route path={Paths.CLASS_LIST} element={<ClassListRoot />} />
-      <Route path={Paths.SIGN_IN} element={<SignInRoot />} />
+      <Route path={Paths.SIGN_IN} element={<ComingSoon />} />
+      <Route path={Paths.ABOUT} element={<ComingSoon />} />
       <Route path="*" element={<Navigate to="/" replace={true} />} />
     </Route>
   )
