5c124376c4661fe38af0eb66e428c2dabfbc0225
Abbondanzo <peter@abbondanzo.com>
2023-07-11 20:44:14 -0400

    Fix search params to work with hash routing

--- frontend/src/features/filters/operators/searchParams.ts
+++ frontend/src/features/filters/operators/searchParams.ts
@@ -1,31 +1,37 @@
+const getAllSearchParams = () => {
+  const params = window.location.href.split("?")[1] || "";
+  return new URLSearchParams(params);
+};
+
+const buildUrl = (searchParams: URLSearchParams) => {
+  const searchParamsString = searchParams.toString();
+  const searchParamsSuffix = searchParamsString ? `?${searchParamsString}` : "";
+  return (
+    window.location.protocol +
+    "//" +
+    window.location.host +
+    window.location.pathname +
+    window.location.hash.split("?")[0] +
+    searchParamsSuffix
+  );
+};
+
 export const getSearchParams = (key: string): string | null => {
-  const searchParameters = new URLSearchParams(window.location.search);
+  const searchParameters = getAllSearchParams();
   const maybeValue = searchParameters.get(key);
   return maybeValue === null ? maybeValue : decodeURIComponent(maybeValue);
 };
 
 export const setSearchParams = (key: string, value: string) => {
-  const searchParameters = new URLSearchParams(window.location.search);
+  const searchParameters = getAllSearchParams();
   searchParameters.set(key, encodeURIComponent(value));
-  const url =
-    window.location.protocol +
-    "//" +
-    window.location.host +
-    window.location.pathname +
-    "?" +
-    searchParameters.toString();
+  const url = buildUrl(searchParameters);
   window.history.pushState({ path: url }, "", url);
 };
 
 export const removeSearchParams = (key: string) => {
-  const searchParameters = new URLSearchParams(window.location.search);
+  const searchParameters = getAllSearchParams();
   searchParameters.delete(key);
-  const url =
-    window.location.protocol +
-    "//" +
-    window.location.host +
-    window.location.pathname +
-    "?" +
-    searchParameters.toString();
+  const url = buildUrl(searchParameters);
   window.history.pushState({ path: url }, "", url);
 };
