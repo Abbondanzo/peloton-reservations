5dd4a40324f1147859500ba82f5d4d982cc5c16d
Abbondanzo <peter@abbondanzo.com>
2023-07-24 00:47:11 -0400

    Reset selections upon studio change

--- frontend/src/features/alerts/components/AlertEditor.tsx
+++ frontend/src/features/alerts/components/AlertEditor.tsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 import { STUDIOS } from "../../class-list/constants/studios";
 import { fetchClassList } from "../../class-list/slices/classListSlice";
 import { StudioGroup } from "../../filters/components/StudioGroup";
-import { useAppDispatch } from "../../store/hooks/useStore";
+import { useAppDispatch, useAppSelector } from "../../store/hooks/useStore";
 import { Padding } from "../../theme/components/Padding";
 import { SectionTitle } from "../../theme/components/SectionTitle";
 import { InstructorsPicker } from "./editor/InstructorsPicker";
 import { DisciplinesPicker } from "./editor/DisciplinesPicker";
+import { selectStudio } from "../../class-list/selectors/selectStudio";
 
 export const AlertEditor = () => {
   const dispatch = useAppDispatch();
@@ -16,6 +17,7 @@ export const AlertEditor = () => {
     )!;
     dispatch(fetchClassList(classId));
   }, [dispatch]);
+  const selectedStudio = useAppSelector(selectStudio);
 
   const [selectedInstructors, setSelectedInstructors] = useState<
     string[] | null
@@ -23,6 +25,12 @@ export const AlertEditor = () => {
   const [selectedDisciplines, setSelectedDisciplines] = useState<
     string[] | null
   >(null);
+
+  // Reset all picked disciplines and instructors when the studio selection changes
+  useEffect(() => {
+    setSelectedInstructors((cur) => (cur ? [] : cur));
+    setSelectedDisciplines((cur) => (cur ? [] : cur));
+  }, [selectedStudio]);
 
   return (
     <form>
