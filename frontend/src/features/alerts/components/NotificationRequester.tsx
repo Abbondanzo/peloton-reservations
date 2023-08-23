7aaaa93aeb1e7819390a74e85e93413f40bd18e7
Abbondanzo <peter@abbondanzo.com>
2023-08-23 00:35:15 -0400

    Check for Notification reference

--- frontend/src/features/alerts/components/NotificationRequester.tsx
+++ frontend/src/features/alerts/components/NotificationRequester.tsx
@@ -13,18 +13,37 @@ const ButtonWithTopMargin = styled(Button)`
 `;
 
 export const NotificationRequester = () => {
-  const [permission, setPermission] = useState(Notification.permission);
+  const [permission, setPermission] = useState<
+    NotificationPermission | undefined
+  >(window.Notification ? window.Notification.permission : undefined);
 
   const requestPermission = useCallback(() => {
-    Notification.requestPermission()
-      .then((permission) => {
-        setPermission(permission);
-      })
-      .catch((error) => {
-        console.error(error);
-        captureException(error);
-      });
+    if (
+      window.Notification &&
+      typeof window.Notification.requestPermission === "function"
+    ) {
+      window.Notification.requestPermission()
+        .then((permission) => {
+          setPermission(permission);
+        })
+        .catch((error) => {
+          console.error(error);
+          captureException(error);
+        });
+    }
   }, []);
+
+  if (permission === undefined) {
+    return (
+      <CardWithBottomMargin>
+        <h2>Unsupported Browser</h2>
+        <p>
+          This browser does not support the notification API and we cannot
+          notify you using it
+        </p>
+      </CardWithBottomMargin>
+    );
+  }
 
   if (permission !== "granted") {
     return (
