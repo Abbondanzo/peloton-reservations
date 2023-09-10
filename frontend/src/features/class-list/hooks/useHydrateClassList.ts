88edd8fed023af1f74de38d592de769776edda92
Abbondanzo <peter@abbondanzo.com>
2023-09-10 13:33:57 -0400

    Prevent refetching

--- frontend/src/features/class-list/hooks/useHydrateClassList.ts
+++ frontend/src/features/class-list/hooks/useHydrateClassList.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/hooks/useStore";
 import { selectActiveClassList } from "../selectors/selectActiveClassList";
 import { selectStudioId } from "../selectors/selectStudioId";
@@ -9,9 +9,14 @@ export const useHydrateClassList = () => {
   const currentStudioId = useAppSelector(selectStudioId);
   const currentState = useAppSelector(selectActiveClassList);
   const currentStatus = currentState?.status;
+  const hasFetchedRef = useRef(false);
 
   useEffect(() => {
-    if (!currentStatus || currentStatus !== "loading") {
+    if (
+      !hasFetchedRef.current &&
+      (!currentStatus || currentStatus !== "loading")
+    ) {
+      hasFetchedRef.current = true;
       dispatch(fetchClassList(currentStudioId));
     }
   }, [currentStatus, currentStudioId, dispatch]);
