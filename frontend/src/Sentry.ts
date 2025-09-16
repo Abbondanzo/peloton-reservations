25d0793af72998f40289cf8a44a0f1685581d53e
Peter Abbondanzo <peter@abbondanzo.com>
2025-09-15 22:45:32 -0400

    Upgrade to latest Sentry, toolkit, styles components, and vitals

--- frontend/src/Sentry.ts
+++ frontend/src/Sentry.ts
@@ -10,15 +10,16 @@ export const initialize = () => {
 
   Sentry.init({
     environment: process.env.NODE_ENV,
-    dsn: "https://a9efe74598ad4f0b86604e9694d254ed@o237935.ingest.sentry.io/4505446442663936",
+    dsn: "https://a9efe74598ad4f0b86604e9694d254ed@o237935.ingest.us.sentry.io/4505446442663936",
     integrations: [
-      new Sentry.BrowserTracing({
-        // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-        tracePropagationTargets: ["localhost"],
-      }),
-      new Sentry.Replay(),
+      Sentry.browserTracingIntegration(),
+      Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+      Sentry.replayIntegration(),
     ],
+    // Logging
+    enableLogs: true,
     // Performance Monitoring
+    tracePropagationTargets: ["localhost"],
     tracesSampleRate: sampleRate,
     // Session Replay
     replaysSessionSampleRate: sampleRate,
