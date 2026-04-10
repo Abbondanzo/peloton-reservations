edd644d4c7f2b210b76be40c78c2b2dfa7aa6403
Peter Abbondanzo <peter@abbondanzo.com>
2026-04-10 07:47:03 -0400

    Modernize sign in

--- frontend/src/features/session/components/SignInRoot.tsx
+++ frontend/src/features/session/components/SignInRoot.tsx
@@ -4,14 +4,33 @@ import { SignIn } from "../../firebase/components/SignIn";
 import { NavbarProvider } from "../../navigation/components/NavbarProvider";
 import { Paths } from "../../navigation/constants/paths";
 import { useAppSelector } from "../../store/hooks/useStore";
-import { Card } from "../../theme/components/Card";
+import { mediaMobile } from "../../theme/constants/queries";
 import { selectHasSession } from "../selectors/selectHasSession";
 
-const Wrapper = styled.div`
+const PageWrapper = styled.div`
+  min-height: calc(100vh - 60px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 24px 20px;
+
+  ${mediaMobile`
+    align-items: flex-start;
+    padding: 16px 12px;
+  `}
+`;
+
+const Card = styled.div`
+  width: 100%;
   max-width: 400px;
-  padding: 1em;
-  margin: 0 auto;
-  text-align: center;
+  background-color: ${(p) => p.theme.colors.mainSurface};
+  border: 1px solid ${(p) => p.theme.borderColor};
+  border-radius: ${(p) => p.theme.borderRadius};
+  padding: 32px;
+
+  ${mediaMobile`
+    padding: 24px 20px;
+  `}
 `;
 
 export const SignInRoot = () => {
@@ -21,11 +40,11 @@ export const SignInRoot = () => {
   }
   return (
     <NavbarProvider>
-      <Wrapper>
+      <PageWrapper>
         <Card>
           <SignIn />
         </Card>
-      </Wrapper>
+      </PageWrapper>
     </NavbarProvider>
   );
 };
